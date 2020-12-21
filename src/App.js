eab61717754b84e62ec9e9948e1cd1d33e9121e4
Souheil Kooli <kooli_shouheil@yahoo.fr>
2020-12-21 12:32:19 +0100

    4

--- src/App.js
+++ src/App.js
@@ -90,15 +90,7 @@ function App() {
       <option>2020</option>
       <option>2019</option>
       <option>2018</option>
-      <option>2017</option>
-      <option>2016</option>
-      <option>2015</option>
-      <option>2014</option>
-      <option>2013</option>
-      <option>2012</option>
-      <option>2011</option>
-      <option>2010</option>
-      <option>2009</option>
+     
     
     </Form.Control>
   </Form.Group>
